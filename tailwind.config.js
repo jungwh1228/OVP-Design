725de589f1603e1d28a954cabeeefc8f9a4f09a5
jungseolhwa <dhqkfskrwl@naver.com>
2024-06-17 17:27:37 +0900

    폰트추가, 컴포넌트 유형 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
     "./assets/scss/tailwind/**/*.scss"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'pretendard': ['Pretendard', 'Segoe UI', 'Open Sans', 'Helvetica Neue'],
-        'mono': ['Consolas', 'Menlo', 'Courier', 'monospace']
-      },
-    }
   },
   plugins: [],
 }
